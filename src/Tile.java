512dd2cf2b02d76db227fd491c92eab23d9964f4
Gian Paul Ramirez <35239075+paul-rl@users.noreply.github.com>
2022-09-01 16:45:20 -0700

    Created Tile object

--- src/Tile.java
+++ src/Tile.java
@@ -3,9 +3,10 @@ import javax.rmi.CORBA.Util;
 public class Tile {
     final char letter;
     final int points;
+    final char blankLetter;
 
     public Tile(char letter){
         this.letter = letter;
-        this.points = Utils.(letter);
+        this.points = Utils.getValue(letter);
     }
 }
