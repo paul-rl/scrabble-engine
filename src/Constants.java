7df2cee31b54aa5bd0183be817863350ddc68731
paul-rl <35239075+paul-rl@users.noreply.github.com>
2022-09-02 02:20:06 -0400

    Added board dimension and special tile constants

--- src/Constants.java
+++ src/Constants.java
@@ -1,5 +1,11 @@
 public class Constants {
-    final static char EMPTY_SPOT = '1';
+    final static int BOARD_LENGTH = 15;
+
+    // Tile features
     final static String NO_FEATURE = "NONE";
-    final static
+    final static String TRIPLE_WORD = "3W";
+    final static String DOUBLE_WORD = "2W";
+    final static String TRIPLE_LETTER = "3L";
+    final static String DOUBLE_LETTER = "2L";
+    final static String START = "S";
 }
