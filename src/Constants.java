dac91c4fae2971c1cc193140d7462cebb046806b
paul-rl <35239075+paul-rl@users.noreply.github.com>
2022-09-21 01:31:52 -0400

    Updated constants to include players and valid letters

--- src/Constants.java
+++ src/Constants.java
@@ -1,6 +1,10 @@
 public class Constants {
     final static int BOARD_LENGTH = 15;
 
+    // Letters
+    final static char[] VALID_LETTERS = {'a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                                         'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y',
+                                         'z', '_'};
     // Tile features
     final static String NO_FEATURE = "NONE";
     final static String TRIPLE_WORD = "3W";
@@ -8,4 +12,8 @@ public class Constants {
     final static String TRIPLE_LETTER = "3L";
     final static String DOUBLE_LETTER = "2L";
     final static String START = "S";
+
+    // Player Constants
+    final static int PLAYER_ONE = 0;
+    final static int PLAYER_TWO = 1;
 }
